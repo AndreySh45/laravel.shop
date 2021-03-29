4b19c0ca48b78ef9457e54e652b60079cc2db754
AndreySh45 <and45@mail.ru>
2021-03-29 21:54:15 +0500

    showCategory

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -12,4 +13,9 @@ class ProductController extends Controller
 
         return view('product.show', compact('item'));
     }
+    public function showCategory(Request $request, $cat_alias){
+        $cat = Category::where('alias',$cat_alias)->first();
+        return view('categories.index', compact('cat'));
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'] );
 Route::get('/{cat}/{product_id}', [ProductController::class, 'show'] )->name('showProduct');
+Route::get('/{cat}', [ProductController::class, 'showCategory'] )->name('showCategory');
